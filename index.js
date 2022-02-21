3661320388787d5a9b1349a8df1c2843e0c7ee6d
Andre Luong <andreluong1@gmail.com>
2022-02-21 14:50:10 -0800

    add database

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
+const express = require('express');
+const res = require('express/lib/response');
+const { redirect } = require('express/lib/response');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+var app = express();
+  
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-
-var app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-.get('/db', async (req, res) => {
+app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM usr');
