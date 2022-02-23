a8b242554f3c4895f4511d57809a7e65fbe04982
Brian <le.brian125@gmail.com>
2022-02-23 15:53:40 -0800

    image display

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const pool = new Pool({
 //   }
 
 var app = express()
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
