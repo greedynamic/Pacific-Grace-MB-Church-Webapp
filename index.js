5c5e19e8ca8c3acd1d27619210ece5c5a8df6b01
Brian <le.brian125@gmail.com>
2022-02-22 19:54:27 -0800

    installing nodemon

--- index.js
+++ index.js
@@ -21,3 +21,4 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/login', (req, res) => res.render('pages/login'))
