9dfe495339a983a78343a62b8fc542e05a5d49ca
Brian <le.brian125@gmail.com>
2022-02-26 13:49:15 -0800

    merge

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
+const express = require('express');
+const res = require('express/lib/response');
+const { redirect } = require('express/lib/response');
+const blogRoute = require('./routes/adminBlog');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: 'postgres://postgres:killllik@localhost/users' 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-// || process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+var app = express();
 
-var app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use('/blog', blogRoute);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/database', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`select * from usr`);
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-app.get('/db', async (req,res)=>{
- try {
-     const result = await pool.query(`SELECT * FROM usr`);
-     const data = { results : result.rows };
-     res.render('pages/db', data);
- }
- catch (error) {
-      res.end(error);
- }
-})
+app.get('/signup', (req,res) => res.render('pages/signup'));
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/login', (req,res) => res.render('pages/login'));
+
+app.post('/login', async (req,res) => {
+  try {
+    var email = req.body.email;
+    var password = req.body.password;
+    // gets the password from a given email
+    var loginQuery = `select password from usr where exists (select * from usr where email=${email})`;
+
+    const client = await pool.connect();
+    await client.query(loginQuery); 
+    const results = {'results': (result) ? result.rows : null};
+
+    if (results.length == 1 && results[0] == password) {
+      res.render("pages/db");
+    } else {
+      // failed password
+      console.log(results.length);
+      res.render("pages/signup");
+    }
+  client.release();
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
