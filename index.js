e35c193ebd19cacebba2daab1dcc1ca7951810ca
Anh Vo <hoanganhvo.162@gmail.com>
2022-02-26 00:22:21 -0800

    add routes for adminBlog

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
+const express = require('express');
+const res = require('express/lib/response');
+const { redirect } = require('express/lib/response');
+const blogRoute = require('./routes/adminBlog');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+var app = express();
 
-const express = require('express')
-const createBlogRouter = require('./routes/blogs/createBlog')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use('/blog', blogRoute);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-dotenv.config();
-mongoose.connect(process.env.BLOG_DATABASE_URI, {
-  useNewUrlParser:true,
-  useUnifiedTopology: true,
-}).then(console.log("Connected to BlogDB")).catch((err) => console.log(err));
+app.get('/database', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`select * from usr`);
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-const app = express()
-app.use(express.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/signup', (req,res) => res.render('pages/signup'));
+
+app.get('/login', (req,res) => res.render('pages/login'));
+
+app.post('/login', async (req,res) => {
+  try {
+    var email = req.body.email;
+    var password = req.body.password;
+    // gets the password from a given email
+    var loginQuery = `select password from usr where exists (select * from usr where email=${email})`;
+
+    const client = await pool.connect();
+    await client.query(loginQuery); 
+    const results = {'results': (result) ? result.rows : null};
+
+    if (results.length == 1 && results[0] == password) {
+      res.render("pages/db");
+    } else {
+      // failed password
+      console.log(results.length);
+      res.render("pages/signup");
+    }
+  client.release();
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 
-app.use('/blogs', createBlogRouter)
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
