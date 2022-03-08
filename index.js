8cd0d529093bfcebbf6e5a50ca6ccbd76cf4bad5
Brian <le.brian125@gmail.com>
2022-03-07 23:12:39 -0800

    readding home redirect that got removed for some reason

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use('/blog', blogRoute);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req,res) => res.render('pages/homepage'));
 app.get('/database', async (req, res) => {
   try {
     const client = await pool.connect();
