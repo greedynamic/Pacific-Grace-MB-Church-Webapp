dc786f9342b5d40276aaa65cb352916bdb29fcca
Brian <le.brian125@gmail.com>
2022-04-07 19:58:08 -0700

    organizing tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,6 +7,36 @@ const server = require('../index')
 
 chai.use(chaiHttp)
 
+describe('/signup', () => {
+    it('should GET signup page', (done) => {
+        chai.request(server)
+            .get("/signup")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+    it('should reject invalid input', (done) => {
+        chai.request(server)
+            .get("/signup")
+            .send({'email':'notanemail', 'password':'bad'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+    it('signup should finish after input', (done) => {
+        chai.request(server)
+            .post("/signup")
+            .end((err, res) => {
+                res.should.be.string;
+                res.should.have.status(200);
+                done();
+            })
+    })
+})
 describe('/login', () => {
     it('should GET login page', (done) => {
         chai.request(server)
